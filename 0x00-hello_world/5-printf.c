c0db718746086aec5729325c0fe2be87242ae5d9
Trmrskr <simonalale@gmail.com>
2022-06-30 11:14:05 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ *Description
+ * */
+int main(void)
+{
+	/*Description*/
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	/*Description*/
+	return (0);
+}
+
