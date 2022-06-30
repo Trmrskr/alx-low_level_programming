58e76f44c8b9350e81144485507103c53b9c05db
Trmrskr <simonalale@gmail.com>
2022-06-30 11:59:29 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 /**
  *main - entry point of the program
- *return - always 0 on success success
+ *return: 0 on success
  */
 int main(void)
 {
 	/*Description*/
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	/**
-	 *return - return statement
-	 *0 - value 
-	 */
 	return (0);
 }
