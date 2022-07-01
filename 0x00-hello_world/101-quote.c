2e8317be066ce8157380ef6e92b6bc8a38823444
Trmrskr <simonalale@gmail.com>
2022-06-30 21:44:53 -0700

    A script that generates the intel syntax of an assembly code of a C program and output to a file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a string to standard error.
+ * Return: 1 on success.
+ */
+
+int main(void)
+{
+	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
