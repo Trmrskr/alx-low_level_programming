79f1bd5136ab7109164fe5fd6ae3153294fb2f3d
Trmrskr <simonalale@gmail.com>
2022-07-01 08:07:51 -0700

    C program to test if last digit of a number is >5, <6 or equal to 0

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int n;
+	int last_digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/* your code goes there */
+
+	last_digit = n % 10;
+
+	if (last_digit > 5)
+	{
+		printf("Last digit of %d is %d and is greater \
+than 5", n, last_digit);
+	}
+	else if (last_digit < 6 && last_digit != 0)
+	{
+		printf("Last digit of %d is %d and is less \
+than 6 and not zero", n, last_digit);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0", n, last_digit);
+	}
+
+	return (0);
+
+}
