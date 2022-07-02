93062bed272b791606367a0e1c37dde743d093c5
Trmrskr <simonalale@gmail.com>
2022-07-01 22:45:39 -0700

    A c program that prints all combination of 3 digit numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int i = 0;
+	int hundredth_digit, tenth_digit, unit_digit;
+
+	while (i < 1000)
+	{
+		hundredth_digit = i / 100;
+		tenth_digit = (i / 10) % 10;
+		unit_digit = i % 10;
+
+		if ((hundredth_digit < tenth_digit) && (hundredth_digit < unit_digit))
+		{
+			if(tenth_digit < unit_digit)
+			{
+				putchar(hundredth_digit + '0');
+				putchar(tenth_digit + '0');
+				putchar(unit_digit + '0');
+
+				if (i != 789)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		i += 1;
+	}
+	putchar('\n');
+	return (0);
+}
