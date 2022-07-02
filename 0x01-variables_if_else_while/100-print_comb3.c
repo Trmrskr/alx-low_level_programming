c9a62c13429f20ee6e72d2fec802f3a1dc0594de
Trmrskr <simonalale@gmail.com>
2022-07-01 21:44:42 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int i = 0;
+	int last_digit, first_digit;
+
+	while (i < 100)
+	{
+		last_digit = i % 10;
+		first_digit = i / 10;
+
+		if (last_digit > first_digit)
+		{
+			putchar(first_digit + '0');
+			putchar(last_digit + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
