fdd1e390fc4df02442f2d898416f4b2b04db5411
Trmrskr <simonalale@gmail.com>
2022-07-02 00:40:19 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,12 +14,12 @@ int main(void)
 		for (j = 0; j < 100; j++)
 		{
 			int i_1st_digit, i_2nd_digit, j_1st_digit, j_2nd_digit;
-			
+
 			i_1st_digit = i / 10;
 			i_2nd_digit = i % 10;
 			j_1st_digit = j / 10;
 			j_2nd_digit = j % 10;
-			
+
 			if (j > i)
 			{
 				putchar(i_1st_digit + '0');
@@ -28,12 +28,11 @@ int main(void)
 				putchar(j_1st_digit + '0');
 				putchar(j_2nd_digit + '0');
 
-				if (i != 98 && j != 99)
+				if (i != 98)
 				{
 					putchar(',');
 					putchar(' ');
 				}
-
 			}
 		}
 	}
