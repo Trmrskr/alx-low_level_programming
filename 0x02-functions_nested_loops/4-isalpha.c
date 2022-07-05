956c9d74b77fb6df39349837f2b753e7c63ceacf
Trmrskr <simonalale@gmail.com>
2022-07-05 14:35:47 -0700

    isalpha?

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+/**
+ * _isalpha - returns 1 if character is alphabet
+ * @c: character to be checked
+ * Return: 1 if true
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') && (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
