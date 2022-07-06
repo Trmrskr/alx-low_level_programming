a490700bef0fad005e7c2c369e7bf1e41da4802f
Trmrskr <simonalale@gmail.com>
2022-07-05 23:44:29 -0700

    Addition function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+ * add - returns the sum of two integers
+ * Return: integer sum if successful
+ * @a: integer to be added
+ * @b: integer to be added
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
