a7938e8e6858955720edc17a1afd4a4ec340c062
Trmrskr <simonalale@gmail.com>
2022-07-07 15:18:12 -0700

    incomplete

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print a straight line
+ * @n: argument
+ */
+
+void print_diagonal(int n)
+{
+	if (n >= 0)
+	{
+		int i = 1, j;
+
+		while (i <= n)
+		{
+			j = 1;
+			while (j <= n)
+			{
+				if (i == j)
+				{
+					_putchar('\\');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+				j++;
+			}
+			i++;
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
