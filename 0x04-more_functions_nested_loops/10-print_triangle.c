f8c3cbfc861de66d20b854c0c05d36c631f91f59
Trmrskr <simonalale@gmail.com>
2022-07-07 18:31:10 -0700

    Error: betty correction, line 12 and 17

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,12 +9,13 @@ void print_triangle(int n)
 {
 	int i, j;
 
-	if (n > 0){
+	if (n > 0)
+	{
 		for (i = 1; i <= n; i++)
 		{
 			for (j = 1; j <= n; j++)
 			{
-				if (j <= (n-i))
+				if (j <= (n - i))
 				{
 					_putchar(' ');
 				}
