3b64f41a5a03bb232665a112944efed3563cf37f
Trmrskr <simonalale@gmail.com>
2022-07-07 18:28:46 -0700

    triangle function

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle
+ * @n: arguments
+ */
+
+void print_triangle(int n)
+{
+	int i, j;
+
+	if (n > 0){
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= n; j++)
+			{
+				if (j <= (n-i))
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
