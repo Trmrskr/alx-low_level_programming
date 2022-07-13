4a8d9c2a9467c8e7894c1d10ae38bea6f80afe3f
Trmrskr <simonalale@gmail.com>
2022-07-13 12:13:53 -0700

    current levels

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+
 /**
  * _atoi - convert string to numbers
  * @s: string to be converted.
@@ -6,6 +8,43 @@
 
 int _atoi(char *s)
 {
-	int num = *s - '0';
-	return (num);
+	int *num;
+	int count_minus = 0, num_loop = 0, count_num = 0, i = 0, j, sum = 0, cum_sum = 0;
+	num = (int*)malloc(10 * sizeof(int));
+
+	while (*s != '\0')
+	{
+		if (*s == '-')
+		{
+			count_minus += 1;
+		}
+		if (*s >= '0' && *s <= '9')
+		{
+			num_loop = 1;
+			num[count_num] = *s - '0';
+			count_num += 1;
+		}
+		if (num_loop == 1 && !(*s >= '0' && *s <= '9'))
+			break;
+	}
+
+	while (i < count_num)
+	{
+		j = 0;
+		while (j < count_num)
+		{
+			num[j] *= 10;
+			sum = num[j];
+			j++;
+		}
+		cum_sum += sum;
+		i++;
+	}
+
+	if (count_minus % 2 != 0)
+	{
+		cum_sum *= -1;
+	}
+
+	return (cum_sum);
 }
