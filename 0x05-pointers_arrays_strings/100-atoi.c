d8066e1fa8db7c1d71d59cedaad7f61ef16a978f
Trmrskr <simonalale@gmail.com>
2022-07-13 08:43:05 -0700

    test

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int first_digit = 9;
+	int second_digit = 8;
+	putchar(first_digit + '0');
+	putchar(second_digit + '0');
+	putchar('\n');
+	return (0);
+}
