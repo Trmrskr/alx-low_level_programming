2840b0541e4df93f139f518198f521ae3a0366f6
Trmrskr <simonalale@gmail.com>
2022-07-12 22:39:13 -0700

    Betty corrections made

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,12 +3,14 @@
 
 /**
  * print_array - print n elements of an array
+ * @a: array to be printed
  * @n: the number of element to be printed
  */
 
 void print_array(int *a, int n)
 {
 	int i;
+
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", *(a + i));
