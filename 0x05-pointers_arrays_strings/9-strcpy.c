dbc3591a070c0d8d91ff8570edcd01ed1ab452b7
Trmrskr <simonalale@gmail.com>
2022-07-12 23:07:32 -0700

    copies one string into another

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+
+/**
+ * strcpy - copies exact copy of a string
+ * @dest: destination string
+ * @src: source string
+ * Return: pointer to dest.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != 0)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+
+	return (dest);
+}
