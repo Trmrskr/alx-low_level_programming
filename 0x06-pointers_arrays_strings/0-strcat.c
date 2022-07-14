7c7451aaa4559dc21a196bc394dd1926b9c7d83e
Trmrskr <simonalale@gmail.com>
2022-07-14 10:15:05 -0700

    Concantenate strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * _strcat - returns concatenated string.
+ * @dest: destination string
+ * @src: Source string
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len = 0, i = 0;
+
+	while (dest[i++])
+		len++;
+	
+	i = 0;
+
+	while (src[i])
+		dest[len++] = src[i++];
+
+	return (dest);
+}
