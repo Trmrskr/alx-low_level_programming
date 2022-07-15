63108b79d2db077aa1552286c303bea35058121b
Trmrskr <simonalale@gmail.com>
2022-07-15 07:45:15 -0700

    leet the string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * leet - replacing special characters
+ * @str: the string in which to replace characters
+ * Return: str
+ */
+
+char *leet(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == 'a' || str[i] == 'A')
+		{
+			str[i] = '4';
+		}
+		if (str[i] == 'e' || str[i] == 'E')
+		{
+			str[i] = '3';
+		}
+		if (str[i] == 'o' || str[i] == 'O')
+		{
+			str[i] = '0';
+		}
+		if (str[i] == 't' || str[i] == 'T')
+		{
+			str[i] = '7';
+		}
+		if (str[i] == 'l' || str[i] == 'L')
+		{
+			str[i] = '1';
+		}
+
+		i++;
+	}
+	return (str);
+}
