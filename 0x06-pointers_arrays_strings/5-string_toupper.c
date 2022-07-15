5adfa8bc9d70dbbbbf76684761c6e41a2b00e203
Trmrskr <simonalale@gmail.com>
2022-07-14 21:09:58 -0700

    Betty corrected

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,12 +1,13 @@
 /**
  * string_toupper - convert string letters to upper case
- * @s: string whose letter is to be conveted
+ * @str: string whose letter is to be conveted
  * Return: Converted String.
  */
 
 char *string_toupper(char *str)
 {
 	int i = 0;
+
 	while (str[i] != '\0')
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
