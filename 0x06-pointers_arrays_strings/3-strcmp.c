ff03b0b5b28707b658ec3482495283552a7823f2
Trmrskr <simonalale@gmail.com>
2022-07-15 10:05:38 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,14 +7,12 @@
 
 int *_strcmp(char *s1, char *s2)
 {
-	int i = 0;
 
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while ((*s1 && *s2) && (*s1 == *s2))
 	{
-		if (s1[i] != s2[i])
-			break;
-		i++;
+		s1++;
+		s2++;
 	}
 	
-	return *((int)s1[i] - (int)s2[i]);
+	return *s1 - *s2;
 }
