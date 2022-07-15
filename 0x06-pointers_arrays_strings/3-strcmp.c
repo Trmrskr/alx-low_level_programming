49b634e27185c8edb2a6945ea62e3a1f0e866a07
Trmrskr <simonalale@gmail.com>
2022-07-15 10:16:26 -0700

    changed int *_strcmp to int _strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,17 +2,17 @@
  * _strcmp - function to compare two strings
  * @s1: first string
  * @s2: second string
- * Return: destination
+ * Return: the difference between the 2 last characters indexed
  */
 
-int *_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 
-	while ((*s1 && *s2) && (*s1 == *s2))
+	while (*s1 && (*s1 == *s2))
 	{
 		s1++;
 		s2++;
 	}
 	
-	return *s1 - *s2;
+	return (*s1 - *s2);
 }
