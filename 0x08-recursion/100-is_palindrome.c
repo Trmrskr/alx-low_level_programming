b67cd9420d31db300307183ca7887a41075c3196
Trmrskr <simonalale@gmail.com>
2022-07-21 08:23:00 -0700

    A function that returns tells if a string is a palindrome or not

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,57 @@
+int palinity(char *, int, int);
+int _strlen(char *);
+/**
+ * is_palindrome - test a string if it is palindrome
+ * @s: string to be tested
+ * Return: 1 if true, 0 otherwise
+ */
+
+int is_palindrome(char *s)
+{
+	int i = 0;
+	int len = _strlen(s) - 1;
+
+	if (len <= 1)
+		return (1);
+
+	return (palinity(s, i, len));
+}
+
+/**
+ * palinity - test string further if it is palindrome
+ * @s: string to be tested further
+ * @i: current position of element from start
+ * @len: current position of element from end
+ * Return: 1 if s is palindrome, 0 otherwise
+ */
+
+int palinity(char *s, int i, int len)
+{
+	int mid = (i + len) / 2;
+
+	if (i > mid)
+		return (1);
+
+	if (*(s + i) != *(s + len))
+		return (0);
+
+	return (palinity(s, ++i, ++len));
+}
+
+/**
+ * _strlen - returns the length of the string
+ * @s: string whose length is be returned
+ * Return: length of the string s
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s)
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
