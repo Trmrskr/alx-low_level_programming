7037979ad70a0b00e0cf2c6c6a11820df2741edc
Trmrskr <simonalale@gmail.com>
2022-07-22 16:13:00 -0700

    print number of argument passed to it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: number of main argument
+ * @argv: array vector
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
