b3123ab7734c75be329fde4f028c15cfc4df1650
Trmrskr <simonalale@gmail.com>
2022-07-22 17:08:13 -0700

    A program that multiplies two numbers entered into argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int _atoi(char *);
+
+/**
+ * main - Entry point
+ * @argc: number of main argument
+ * @argv: array vector
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int num_1, num_2;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num_1 = _atoi(argv[1]);
+	num_2 = _atoi(argv[2]);
+	
+	printf("%d\n", num_1 * num_2);
+
+	return (0);
+}
+
+int _atoi(char *s)
+{
+	int sum = 0, i = 0, sign = 1;
+
+	if (s[0] == '-')
+	{
+		sign *= -1;
+		i = 1;
+	}
+
+	while (s[i])
+	{
+		int value = s[i] - '0';
+		sum = (sum * 10) + value;
+		i++;
+	}
+
+	return (sum * sign);
+}
