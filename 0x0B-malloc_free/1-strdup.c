078b2d58763ee2ae1d79bf67d245903b32e83961
Trmrskr <simonalale@gmail.com>
2022-07-25 09:27:14 -0700

    imitation of the strdup function that duplicates strings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+
+int _strlen(char *);
+/**
+ * _strdup - Duplicates a string and returns a pointer to the new string
+ * str - string
+ * Return: Address of duplicate string
+ */
+
+char *_strdup(char *str)
+{
+
+	int len = _strlen(str), i = 0;
+	char *dup_str = (char *)malloc(sizeof(char) * len);
+	
+	for (i = 0; i < len; i++)
+	{
+		dup_str[i] = str[i];
+	}
+	return (dup_str);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string which length is to be returned
+ * return: integer
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s)
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
