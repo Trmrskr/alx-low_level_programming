3388ea113aefb27cdee89e1e34221d54356e51f6
Trmrskr <simonalale@gmail.com>
2022-07-27 09:03:49 -0700

    a few adjustements

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -60,7 +60,7 @@ char **char_space_alloc(char *str, char **new_str)
 		{
 			char_count += 1;
 		}
-		if (word == 1 && str[i] == ' ')
+		if (word == 1 && (str[i] == ' '|| str[i + 1] == '\0'))
 		{
 			word = 0;
 			new_str[word_count] = (char *)malloc(sizeof(char) * char_count);
