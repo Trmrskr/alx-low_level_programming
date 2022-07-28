de1442ce754ce0a920cae9bf64fdc43516b0f5fe
Trmrskr <simonalale@gmail.com>
2022-07-28 03:31:26 -0700

    contracted if statement to exclude some more characters

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -69,7 +69,7 @@ void print_char(char *b, int i, int size)
 		if (i >= size)
 			break;
 
-		if (b[i] >= 0 && b[i] <= 31)
+		if ((b[i] >= 0 && b[i] <= 31) && b[i] <= 126)
 			printf(".");
 		else
 			printf("%c", b[i]);
