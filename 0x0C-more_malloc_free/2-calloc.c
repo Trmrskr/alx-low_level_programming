3ffd6397a0fa23878f78cf3b7a3477d1f00698e7
Trmrskr <simonalale@gmail.com>
2022-07-28 20:30:54 -0700

    calloc function

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,6 +9,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *alloc;
+	char *buffer;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
@@ -18,9 +19,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (alloc == NULL)
 		return (NULL);
+	
+	buffer = alloc;
 
 	for (i = 0; i < (size * nmemb); i++)
-		alloc[i] = '\0';
+		buffer[i] = '\0';
 
 	return (alloc);
 }
