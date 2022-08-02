35750b547e9fbeb6280f6ecd9692c34fb93fe15b
Trmrskr <simonalale@gmail.com>
2022-08-02 05:04:54 -0700

    Betty Error correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - the dog struct
  * @name: name of dog
@@ -5,8 +7,11 @@
  * @owner: owner of dog
  */
 
-struct dog{
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif /* DOG_H */
