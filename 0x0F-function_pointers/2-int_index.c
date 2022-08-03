af79eaff2442ef90797700b595ed707c8f32992d
Trmrskr <simonalale@gmail.com>
2022-08-03 10:36:32 -0700

    A function taht searches for a string

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include <stddef.h>
+
+/**
+ * int_index - function that searcheds for an integer
+ * @array: array to be searched
+ * @size: size of array
+ * @cmp: pointer to function compare
+ * Return: index on succes, -1 on failure
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index = 0;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	if (cmp != NULL)
+	{
+		while (array[index])
+		{
+			if (cmp(array[index] != 0)
+				return (index);
+			index++;
+		}
+	}
+	return (-1);
+}
