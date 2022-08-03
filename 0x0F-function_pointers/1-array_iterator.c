f3f1a7b375a56a74b43e2cb64f034981803826ff
Trmrskr <simonalale@gmail.com>
2022-08-03 09:33:01 -0700

    Create a function given as a parameter on each element of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * array_iterator - the function that executes a function given as a parameter on each element of an array
+ * @array: array which element are to be passed to function pointer
+ * @size: the size of the array
+ * @action: function pointer to process elment of array
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
 #ifndef FUNC_POINTER_H
 #define FUNC_POINTER_H
+#include <stddef.h>
 
 void _putchar(char);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *, size_t size, void (*action)(int));
 
 #endif /* FUNC_POINTER_H */
