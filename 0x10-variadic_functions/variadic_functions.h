2afbb75e1c2c83ba133a154f7906f40e58bfa43b
Trmrskr <simonalale@gmail.com>
2022-08-05 01:18:18 -0700

    Betty corrections

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,13 +1,20 @@
-#include <stdarg.h>
 #ifndef VARIADIC_FUNCTION_H
 #define VARIADIC_FUNCTION_H
+#include <stdarg.h>
 
 void _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 
-typedef struct operation{
+/**
+ * struct operation - struct declaration
+ * @op: specifier
+ * @print_variable: function to operate on specifier
+ */
+
+typedef struct operation
+{
 	char op;
 	void (*print_variable)(va_list arg);
 } opt;
