a184ce828d6f72ff155620bbd95cd7e83b61be08
Trmrskr <simonalale@gmail.com>
2022-08-12 10:07:24 +0000

    a function to free memory of list nodes and its property

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - free a list
+ * @head: list to be freed
+ */
+
+void free_list(list_t *head)
+{
+	while(head != NULL)
+	{
+		free(head->str);
+		free(head);
+
+		head = head->next;
+	}
+}
