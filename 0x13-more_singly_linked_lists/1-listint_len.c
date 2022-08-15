40f09833db33b037cc3eba22c55c2dafc5d770bb
Trmrskr <simonalale@gmail.com>
2022-08-15 14:11:13 +0000

    Betty correction

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,7 +2,8 @@
 #include "lists.h"
 
 /**
- * listint_len - a function that returns the number of elements in a linked list
+ * listint_len - a function that returns the number of elements in a linked
+ * list
  * @h: linked list whose number of elements is to be returned
  * Return: the number of elements in the linked list
  */
