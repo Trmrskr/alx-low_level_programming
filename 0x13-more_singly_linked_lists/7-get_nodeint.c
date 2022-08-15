01d72cc70a176de2a1dbf4cf59c71b748739f00d
Trmrskr <simonalale@gmail.com>
2022-08-15 18:23:06 +0000

    Betty correction

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,13 +4,14 @@
 /**
  * get_nodeint_at_index - function to get node at a particular index
  * @head: head of list
+ * @index: the index of the node to be returned
  * Return: the node at the index given.
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	size_t i = 0;
-	
+
 	while (head != NULL)
 	{
 		if (i == index)
