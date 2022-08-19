7fd4a2a472f0e9a7ded891261a88c7cd6bc0d347
Trmrskr <simonalale@gmail.com>
2022-08-19 04:46:01 +0000

    Task 2 done

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,10 +4,12 @@
  * get_bit - get bit of a number at an index
  * @n: number whose bit is to be gotten
  * @index: index from which to get bit
+ * Return: the bit at the index given
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int bit;
+
 	bit = (n >> index);
 	if (index > 32)
 		return (-1);
