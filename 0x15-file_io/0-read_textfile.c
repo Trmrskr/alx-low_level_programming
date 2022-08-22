e89224b7d9e87896452c527c140ca56532fe28fc
Trmrskr <simonalale@gmail.com>
2022-08-22 16:41:45 -0700

    Create a function that reads a text file and p rints it to the standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,41 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - read text from a file and prints to std output
+ * @filename: file to read from
+ * @letters: the number of letter to be read or printed
+ * Return: The  number of letters it could read or print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t r, w;
+	char buff[letters];
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	r = read(fd, buff, letters);
+
+	if (r == -1)
+		return (0);
+
+	w = write(STDOUT_FILENO, buff, r);
+
+	if (w == -1 || w != r)
+		return (0);
+
+	close(fd);
+
+	return (w);
+}
