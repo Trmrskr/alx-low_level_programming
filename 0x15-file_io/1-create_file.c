7b58f8ac8b913ec68ef5409221dcfd061aedf0b5
Trmrskr <simonalale@gmail.com>
2022-08-22 17:39:41 -0700

    A function that crates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * create_file - a function that creates a file
+ * @filename: the name of file to be created.
+ * @text_content: the content to write to the file.
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t fd, w;
+	int len = strlen(text_content);
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		fd = open(filename, O_CREAT | O_WRONLY, 0600);
+
+		if (fd == -1)
+			return (-1);
+		w = write(fd, text_content, len);
+
+		if (w == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
