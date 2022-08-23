2b74a4dd093c26613d934063c5223b55f11928a4
Trmrskr <simonalale@gmail.com>
2022-08-22 18:39:33 -0700

    Betty correction

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,10 +14,10 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	ssize_t fd, w;
 	unsigned int len = 0;
-	
+
 	if (filename == NULL)
 		return (-1);
-	
+
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
