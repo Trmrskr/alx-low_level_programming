e48c5e4c3bddec1bed5c9d6f4f0810a96a489d7a
trmrskr <simonalale@gmail.com>
2022-09-29 07:53:04 -0700

    print all element of a linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,12 +2,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * print_dlistint - print content of a doubly linked list
+ * @h: the head of the list to be printed
+ * Return: count of node
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t node_cnt = 0;
 
-	if (h == NULL)
-		return (node_cnt);
 	while (h != NULL)
 	{
 		printf("%u\n", h->n);
