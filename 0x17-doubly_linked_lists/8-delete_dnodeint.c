e520a6eabac51d60d3f5b75d232f4361e282423d
trmrskr <simonalale@gmail.com>
2022-09-29 22:09:22 -0700

    good

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - delete node at an index
+ * @head: head of node to be deleteed
+ * @index: the index to be deleted rom
+ * Return: 1, or -1
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index){
+	return (1);
+}
