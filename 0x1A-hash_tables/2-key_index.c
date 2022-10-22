499fe79c09243ae6f2f0a3714c5f1708516ee9ee
trmrskr <simonalale@gmail.com>
2022-10-22 11:17:32 -0700

    key->index: betty correction

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,6 +4,7 @@
  * key_index - uses the djb2 algorithm to generate the key index of the string
  * @key: the string to use to generate the key index
  * @size: the size of the hash table
+ * Return: the index in the hash table to which to insert
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
