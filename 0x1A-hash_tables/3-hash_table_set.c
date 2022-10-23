1427afb26f79c0fdd9e0f47c8e70f431fc46dd64
trmrskr <simonalale@gmail.com>
2022-10-23 10:32:55 -0700

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,8 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node, *new_node;
 	int idx = key_index((const unsigned char *)key, ht->size);
 
-	if (ht == NULL || key == NULL || *key == '\0' || value == NULL ||
-*value == '\0')
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
 	new_node = malloc(sizeof(hash_node_t));
@@ -32,7 +31,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[idx] = new_node;
 		return (1);
 	}
-	/* loop through list to check if key already exists */
+	/**
+	 * loop through list, if key already exists simply replace the
+	 * corresponding value
+	 */
 	while (node != NULL)
 	{
 		if (strcmp(node->key, key) == 0)
